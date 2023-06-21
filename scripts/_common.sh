204a4bd2ab64a88ab66f2af28892509f6fe72a7e
Carlos Solís <csolisr@azkware.net>
2023-06-21 17:39:49 -0600

    fix: add more deployment steps to the scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,15 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
+nodejs_version="16"
+mercure_version="0.14.10"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
+email=$(ynh_user_get_info --username=$admin --key="mail")
+app_secret=$(ynh_string_random --length=32 --filter="0-9a-f")
+mercure_jwt_secret=$(ynh_string_random --length=24)
+caddy_mercure_jwt_secret=$(ynh_string_random --length=24)
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
